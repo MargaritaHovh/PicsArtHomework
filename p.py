9a3fc4370168bb8b59df3eefb33c2d24697a9e2c
MargaritaHovh <123767297+MargaritaHovh@users.noreply.github.com>
2023-01-31 17:25:53 +0400

    Add files via upload

---
+++ p.py
@@ -0,0 +1,7 @@
+ char = input("Enter the character: ")
+ if char in ('a', 'e', 'i', 'o', 'u'):
+     print(char,"is a vowel")
+ elif char in ("A","E","I","O","U"):
+     print(char,"is a vowel")
+ else:
+     print(char,"is consonant")
